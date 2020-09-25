05327507f392ee6b436faebfbe360df6e881ae6e
pavelsumanoo <sumanoop@gmail.com>
2020-09-25 18:27:48 -0500

    users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def hello
+    @credentials = Rails.application.credentials.hello
     pet = Pet.first
     @name = pet.name
     @breed = pet.breed
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_044104) do
+ActiveRecord::Schema.define(version: 2020_09_16_225940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "decription"
+    t.string "isbn"
+    t.integer "pages_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "addres"
+    t.text "about_me"
+    t.integer "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
